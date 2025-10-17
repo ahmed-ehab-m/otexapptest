d8bce3e544714cb9f85246b37b896108ba6d416a
Ahmed Ehab <ahmedehab56200@gmail.com>
2025-10-17 19:57:24 +0200

    create clothes entity

---
+++ lib/features/home/domain/entities/clothes_entity.dart
@@ -0,0 +1,13 @@
+class ClothesEntity {
+  final String name;
+  final String image;
+  final double price;
+  final double numberOfSales;
+
+  ClothesEntity({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.numberOfSales,
+  });
+}
